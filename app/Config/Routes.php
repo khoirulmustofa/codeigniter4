8adb10aec6bcafe953459968a2e1869397cfdd82
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2024-05-03 09:38:50 +0700

    Install Shiled

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+service('auth')->routes($routes);
 $routes->get('/', 'Home::index');
+
+$routes->group('dashboard', ['namespace' => 'App\Controllers\Dashboard'], function ($routes) {
+    $routes->get('/dashboard', 'HomeController::index');
+});
